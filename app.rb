56b70b565730490c017fd88505a768a830ef0d05
ada <ada@tim-laptop.(none)>
2010-02-19 23:05:18 +0800

    reply

--- app.rb
+++ app.rb
@@ -5,7 +5,23 @@ post '/' do
   builder do |xml|
     xml.instruct!
     xml.Response do 
-      xml.Say("Hello from my Heroku app")
+      xml.Say("Hello hello hello from my Heroku app")
+      xml.Gather(:action=>"/reply")  do
+	xml.Say("Please press 1 or 2")
+      end
+    end
+  end
+end
+
+post '/reply' do
+  builder do |xml|
+    xml.instruct!
+    xml.Response do
+      if params['Digits'] == '1'
+        xml.Say("you pressed one")
+      else
+	xml.Say("you pressed two")
+      end
     end
   end
 end
